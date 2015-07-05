b4c261b5fd9120d56dcc65719b99f6ea083f994d
lenaroter <rother.magdalena@gmail.com>
2015-07-05 21:31:41 +0200

    Add base animals description
    In progress

---
+++ beastbar/animals.py
@@ -0,0 +1,43 @@
+class Animal:
+    pass
+
+class Skunks(Animal):
+    """
+    A skunk expels all animals of the two stronegest
+    currently displayed species, but never other skunks.
+    """
+    pass
+
+class Parrot(Animal):
+    """
+    A parrot shoos out an animal of your choice.
+    """
+    pass
+
+class Kangaroo(Animal):
+    """
+    Jumps over up to two animals.
+    The strength of jumped over animals does not matter.
+    """
+    pass
+
+class Monkey(Animal):
+    """
+    No effect if there are no other monkeys.
+    If there are more than one monkey, hippos and
+    crocodiles get expeled. The last played monkey is placed
+    first, the other monkes go to the position 2, 3, and 4 
+    """
+    pass
+
+class Chameleon(Animal):
+    pass
+
+class Seal(Animal):
+    pass
+
+class Zebra(Animal):
+    pass
+
+class Giraffe(Animal):
+    pass 
