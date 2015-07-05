e24834d019ea1307bdf86e5de4347bed146536e8
krother <krother@academis.eu>
2015-07-05 23:03:36 +0200

    engineered event loop

--- beastbar/frontend.py
+++ beastbar/frontend.py
@@ -7,40 +7,41 @@ ANIMALS = {
     "2": "Parrot",
     }
 
+#TODO: Message interface
+
 class TextFrontend:
 
     def __init__(self):
         self.game = None
 
-    def enter_animal():
+    def enter_animal(self):
         for num in ANIMALS:
             print("%s\t%s" % (num, ANIMALS[num]))
         print("x\tEXIT")
         print("")
         choice = input("")
         animal = ANIMALS.get(choice, 'x')
+        #TODO: raise exception for x
         return animal
 
     def print_queue(self):
         print("\nQueue status: []\n")
 
-    def create_game():
+    def create_game(self):
         self.game = Game()
         self.game.add_player(Player("Kristian"))
         self.game.add_player(Player("Lena"))
         self.game.start()
 
-    def run_game(game):
-        while not self.game.ended:
-            animal = "-"
-            for p in game.players:
-                self.print_queue()
-                animal = self.enter_animal(player)
-                if animal != 'x':
-                    print ("a %s enters the queue." % animal)
-                    player.play_card(animal)
+    def run_game(self):
+        while self.game.is_running():
+            player = self.game.next_player()
+            self.print_queue()
+            animal = self.enter_animal(player)
+            print ("a %s enters the queue." % animal)
+            player.play_card(animal)
 
-    def finalize_game(game):
+    def finalize_game(self):
         result = self.game.get_result()
         self.print_result(result)
         self.game.end()
