fc6201ad4eda462910ef6d55e31bcc41417b98f2
lenaroter <rother.magdalena@gmail.com>
2015-07-05 21:42:27 +0200

    Add basic animals description
    In progress

--- beastbar/animals.py
+++ beastbar/animals.py
@@ -16,21 +16,29 @@ class Parrot(Animal):
 
 class Kangaroo(Animal):
     """
-    Jumps over up to two animals.
+    A kangaroo jumps over up to two animals.
     The strength of jumped over animals does not matter.
     """
     pass
 
 class Monkey(Animal):
     """
-    No effect if there are no other monkeys.
+    A single monkey has no effect.
     If there are more than one monkey, hippos and
     crocodiles get expeled. The last played monkey is placed
     first, the other monkes go to the position 2, 3, and 4 
+    in the reverse order.
     """
     pass
 
 class Chameleon(Animal):
+    """
+    A chameleon carries out the action of a species that is
+    present in the queue. For this action chameleon takes on
+    the strength of the imitated species.
+    During execution of recurring actions chameleon is 
+    chameleon again.  
+    """
     pass
 
 class Seal(Animal):
